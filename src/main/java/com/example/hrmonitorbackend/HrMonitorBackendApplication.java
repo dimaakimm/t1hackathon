bc07025c4e3439952bb1fce0fda233443cfcfaee
Vitaliy <70775970+vitaliy-101@users.noreply.github.com>
2024-10-03 15:04:28 +0300

    Start commit

---
+++ src/main/java/com/example/hrmonitorbackend/HrMonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrmonitorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrMonitorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrMonitorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hrmonitorbackend/HrMonitorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrmonitorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrMonitorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
