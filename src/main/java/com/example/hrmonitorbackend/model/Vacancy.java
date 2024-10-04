68172e5656e8982d774f60b7aa769035424bd90b
Vitaliy <70775970+vitaliy-101@users.noreply.github.com>
2024-10-04 20:06:37 +0300

    Start commit backend 3.0

--- src/main/java/com/example/hrmonitorbackend/model/Vacancy.java
+++ src/main/java/com/example/hrmonitorbackend/model/Vacancy.java
@@ -1,13 +1,32 @@
 package com.example.hrmonitorbackend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.hrmonitorbackend.model.auth.Token;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "vacancy")
 public class Vacancy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "name")
+    private Date start_date;
+
+    @Column(name = "name")
+    private Date finish_date;
+
+
+
 }
