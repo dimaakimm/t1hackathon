750ef0c9ddc6dbf29566a7e21633fe87d26360b1
Vitaliy <70775970+vitaliy-101@users.noreply.github.com>
2024-10-04 19:59:17 +0300

    Start commit backend 2.0

--- src/main/java/com/example/hrmonitorbackend/model/Vacancy.java
+++ src/main/java/com/example/hrmonitorbackend/model/Vacancy.java
@@ -1,5 +1,13 @@
 package com.example.hrmonitorbackend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@Table(name = "vacancy")
 public class Vacancy {
 }
